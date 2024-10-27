9b1c77a07dbc710195608859fed17f39a1b9945a
ELANCHELIYAN-2003 <elanon2003@gmail.com>
2024-10-27 23:15:16 +0530

    inital commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { CartComponent } from './cart/cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+//import { OrderTrackComponent } from './order-track/order-track.component';
+import { FilterPipe } from './pipes/filter.pipe';
+import { NgxPayPalModule } from 'ngx-paypal';
+import { OrderedProductsComponent } from './order-track/order-track.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        FooterComponent,
+        ViewProductComponent,
+        CartComponent,
+        WishlistComponent,
+        PagenotfoundComponent,
+        CheckoutComponent,
+        FilterPipe,
+        AppComponent,
+        CheckoutComponent,
+        OrderedProductsComponent,
+        AdminComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgxPayPalModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+export class AppModule {}
